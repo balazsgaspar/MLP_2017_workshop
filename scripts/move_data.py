51b0177f801833ce97e213967529a9901e42d414
karelvaculik <vaculik.dev@gmail.com>
2017-04-24 08:44:02 +0200

    updated links to files

--- scripts/move_data.py
+++ scripts/move_data.py
@@ -12,10 +12,10 @@ sc._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "...")
 sc._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "s3.eu-central-1.amazonaws.com")
 
 
-df1 = sqlContext.read.parquet("s3a://mlp2017/pub/mlp_sampled_cdr_records.parquet")
-df2 = sqlContext.read.parquet("s3a://mlp2017/pub/mlp_sampled_ebr_base_20160401.parquet")
-df3 = sqlContext.read.parquet("s3a://mlp2017/pub/mlp_sampled_ebr_base_20160501.parquet")
-df4 = sqlContext.read.parquet("s3a://mlp2017/pub/mlp_sampled_ebr_churners_20151201_20160630.parquet")
+df1 = sqlContext.read.parquet("s3a://mlprague/pub/mlp_sampled_cdr_records.parquet")
+df2 = sqlContext.read.parquet("s3a://mlprague/pub/mlp_sampled_ebr_base_20160401.parquet")
+df3 = sqlContext.read.parquet("s3a://mlprague/pub/mlp_sampled_ebr_base_20160501.parquet")
+df4 = sqlContext.read.parquet("s3a://mlprague/pub/mlp_sampled_ebr_churners_20151201_20160630.parquet")
 
 
 df1.write.parquet("mlp_sampled_cdr_records.parquet", mode="overwrite")
